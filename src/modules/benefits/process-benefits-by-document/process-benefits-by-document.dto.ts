43185a7e4fe13462cbbc53d43c52d67fc6091a07
IglanCardeal <cmtcardeal@outlook.com>
2024-12-01 00:11:12 -0300

    feat: add document validation

--- src/modules/benefits/process-benefits-by-document/process-benefits-by-document.dto.ts
+++ src/modules/benefits/process-benefits-by-document/process-benefits-by-document.dto.ts
@@ -1,3 +1,13 @@
-export type ProcessBenefitsByDocumentDTO = {
+import { IsArray, ArrayNotEmpty, Matches } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class ProcessBenefitsByDocumentDTO {
+  @IsArray()
+  @ArrayNotEmpty()
+  @Transform(({ value }) => value.map((doc: string) => doc.replace(/\D/g, '')))
+  @Matches(/^\d{11}$/, {
+    each: true,
+    message: 'Each document must have exactly 11 digits',
+  })
   documents: string[];
-};
+}
