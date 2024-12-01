3324a2ee1cc7113d4befda2cee9ceceb67037004
IglanCardeal <cmtcardeal@outlook.com>
2024-12-01 00:53:04 -0300

    feat: add document validation

--- src/modules/benefits/consult-benefits/consult-benefits.dto.ts
+++ src/modules/benefits/consult-benefits/consult-benefits.dto.ts
@@ -1,3 +1,13 @@
-export type ConsultBenefitsDTO = {
+import { Matches, IsString, IsNotEmpty } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class ConsultBenefitsDTO {
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.replace(/\D/g, ''))
+  @Matches(/^\d{11}$/, {
+    each: true,
+    message: 'The document must have exactly 11 digits',
+  })
   document: string;
-};
+}
